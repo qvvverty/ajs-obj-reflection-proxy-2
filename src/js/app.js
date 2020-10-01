d09c6efb95153aeb0d054ad181560870d0420c28
Leonid Kostyuchenko <kstchnko@gmail.com>
2020-10-01 23:22:25 +0300

    getSpecialAttacks mutability fix, destructuring add

--- src/js/app.js
+++ src/js/app.js
@@ -1,8 +1,25 @@
-export default function getSpecialAttacks({ special }) {
-  for (const attack of special) {
-    if (!attack.description) {
-      attack.description = 'Описание недоступно';
+export default function getSpecialAttacks({ special: [...specials] }) {
+  const attacks = [];
+  for (const attack of specials) {
+    const attackCopy = { ...attack };
+    if (!attackCopy.description) {
+      attackCopy.description = 'Описание недоступно';
     }
+    attacks.push(attackCopy);
   }
-  return special;
+  return attacks;
 }
+
+// export default function getSpecialAttacks({ special: [...specials] }) {
+//   const attacks = [];
+
+//   for (const {
+//     id, name, icon, description = 'Описание недоступно',
+//   } of specials) {
+//     attacks.push({
+//       id, name, icon, description,
+//     });
+//   }
+
+//   return attacks;
+// }
